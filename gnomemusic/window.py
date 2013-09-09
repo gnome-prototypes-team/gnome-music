6a53be5586bf73f94de5c7391b511021088302e7
Arnel A. Borja <arnelborja@src.gnome.org>
2013-09-10 00:05:16 +0800

    window: Change sidebar name from 'artists' to 'sidebar'

--- gnomemusic/window.py
+++ gnomemusic/window.py
@@ -195,7 +195,7 @@ class Window(Gtk.ApplicationWindow):
         if stack.get_visible_child() == self.views[1] or \
            stack.get_visible_child() == self.views[3]:
             stack.get_visible_child().stack.set_visible_child_name('dummy')
-            stack.get_visible_child().stack.set_visible_child_name('artists')
+            stack.get_visible_child().stack.set_visible_child_name('sidebar')
         self._show_searchbar(False)
 
     def _toggle_view(self, btn, i):
